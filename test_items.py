09da69f22ff8c757343deff7fb2631fa824dd4f9
Implozzia <ruslan.vafin.93@gmail.com>
2020-07-22 21:20:12 +0300

    Checking the cart button for different languages

---
+++ test_items.py
@@ -0,0 +1,5 @@
+def test_add_to_basket_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    basket = browser.find_element_by_class_name("btn-add-to-basket")
+    assert basket == basket, "Button not found"
